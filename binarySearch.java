fca235d697a113d3d1c0d9cab6e8bb0ae95236fc
Harsha Bheemisetty <bheemisettyh1@udayton.edu>
2022-07-20 20:37:40 -0700

    Binary Search Recursion

--- binarySearch.java
+++ binarySearch.java
@@ -1,17 +1,17 @@
+// Binery Search Recurrsion
 // Here we've given Array of numbers and input number to find out
-public int binarySearch(arr[],x){
-	int start,mid,end;
-	start=0;
-	end=arr.length-1;
-	mid=(start+end)/2;
-	while(start<=end){
-		if(arr[mid]==x) return arr[mid];
-		else if(arr[mid]<x) {
-		start = mid+1;
-		}
-		else end=mid-1;
+public int binarySearch(arr[],x,start,end){
+	if(start > end) return -1; // Base Cond
+	mid = (start + end)//2;
+	if(arr[mid] == x) return arr[mid];
+	else if(arr[mid]>x) {
+		return binarySearch(arr[],x,start,mid-1);
+	else{
+		return binarySearch(arr[],x,mid+1,end);
+
 	}
-	return -1;
+	}
+	
 
 
 }
